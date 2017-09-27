5906cc62fd5adde989541109ad9dea5badcb6fb7
laurkim <laurakeem@gmail.com>
2017-09-27 14:00:49 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,26 @@
+require 'pry'
+
 class MP3Importer
 
   attr_accessor :path
 
   def initialize(path)
     @path = path
+  end
+
+  def files
+      array = Dir.entries(path).map { |file| file if file.end_with?(".mp3")
+    # binding.pry
+  }.compact
+
+  # binding.pry
+  end
+
+  def import
+    self.files.each do |file|
+      Song.new_by_filename(file)
+    # binding.pry
+    end
   end
 
 end
